0e554a57f1b9e355c5c4119444244a51837c4f1b
phuc1111 <phuc.dam@student.passerellesnumeriques.org>
2019-09-11 10:34:09 +0700

    add

--- AndroidTextView/app/src/main/java/com/example/androidtextview/MainActivity.java
+++ AndroidTextView/app/src/main/java/com/example/androidtextview/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.androidtextview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.text);
+        tv.setTextColor(getResources().getColor(R.color.colorPink));
+        tv.setText("Hello Agant");
+    }
+    public void change(){
+
     }
 }
